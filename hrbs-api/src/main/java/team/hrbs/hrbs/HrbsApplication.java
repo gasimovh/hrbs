c44e1b788b0779a50c45a1a9e1ab8e65e22ab043
gasimovh <33291612+gasimovh@users.noreply.github.com>
2020-03-07 18:42:11 +0100

    Intial commit
    setting up the backend

---
+++ hrbs-api/src/main/java/team/hrbs/hrbs/HrbsApplication.java
@@ -0,0 +1,13 @@
+package team.hrbs.hrbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrbsApplication.class, args);
+	}
+
+}
