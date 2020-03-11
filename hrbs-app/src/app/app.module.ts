ea350a9d735d2b028fa012b594a89c242fc747fd
Alper <alpermeydan@mail.ru>
2020-03-11 01:03:09 +0100

    Created component with background for main page

--- hrbs-app/src/app/app.module.ts
+++ hrbs-app/src/app/app.module.ts
@@ -3,13 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomepageComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     AppRoutingModule
   ],
   providers: [],
